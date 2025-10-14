c2bbdcfdce9c01ff1a3aeb3a31ec2f1f2e8bfd5f
Masaaki Komori <gmgateway@gmail.com>
2025-10-15 05:05:55 +0900

    Lint and fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-        <h1 className="text-2xl font-bold">Hello world!</h1>
-      </div>
-    </>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-2xl font-bold">Hello world!</h1>
+    </div>
   );
 }
 
