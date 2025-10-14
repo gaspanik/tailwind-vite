71fb5afe4f7047c1b9551ab43742942b3361b7db
Masaaki Komori <gmgateway@gmail.com>
2025-10-14 21:01:07 +0900

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-2xl font-bold">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
