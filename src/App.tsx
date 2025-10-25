1d017a707e118cd5df7f9c233bd19358b89079b8
Masaaki Komori <gmgateway@gmail.com>
2025-10-25 21:41:34 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { Camera } from 'lucide-react'
+import { SquareDashed } from 'lucide-react'
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="mt-4 flex items-center gap-2">
-        <Camera className="h-6 w-6" />
-        <h1 className="text-2xl font-bold">Hello world!</h1>
+    <div className="flex flex-col justify-center items-center bg-gray-100 min-h-screen">
+      <div className="flex items-center gap-2 mt-4">
+        <SquareDashed className="w-6 h-6" />
+        <h1 className="font-bold text-2xl">Hello world!</h1>
       </div>
     </div>
   )
